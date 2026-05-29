abd28d04a62aabbff3078e261b80f5d22d54db63
mura glim <muraglim@gmail.com>
2026-05-28 23:41:23 -0500

    feat: Roster_Store added
    Roster_Store.tscn: added as child node to Keeper
    Roster_Store.gd: new Store registry for actor state storage.
    
    //
    
    chore: stub Char_Roster_Store.gd deleted

---
+++ core/keeper/roster_store.gd
@@ -0,0 +1,7 @@
+extends Store
+
+func add_to_roster(name: String, id: int, gender: bool, origin: String, zodiac: float) -> void:
+	if has_key(id):
+		push_error("Roster_Store: id already exists - %s" % str(id))
+		return
+	data[id] = {"name": name, "identity": id, "gender": gender, "origin": origin, "zodiac": zodiac, "medals": []}
