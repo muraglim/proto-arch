8f5c9d41f546dae7645e3540f18bb288ba994a99
mura glim <muraglim@gmail.com>
2026-05-31 12:53:19 -0500

    chore: add NavChecker uid
    added to Nav_Store

--- stores/nav_store.gd
+++ stores/nav_store.gd
@@ -6,4 +6,5 @@ func _ready() -> void:
 		"boot_scene": "uid://b27eqwa55glmf",
 		"template_scene": "uid://sew0lmp877f3",
 		"roll_scene": "uid://c431n7ciwstmp",
+		"nav_checker": "uid://cpansnhgsaagn",
 	}
