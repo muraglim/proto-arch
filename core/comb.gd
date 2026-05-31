9f089723ed3bbc1f51d40c76464e24bab25a39ad
mura glim <muraglim@gmail.com>
2026-05-31 05:30:21 -0500

    chore: inline instructions for Comb.valtype
    basic call structure and explanation of function

--- core/comb.gd
+++ core/comb.gd
@@ -6,5 +6,6 @@
 
 extends Node
 
+# Comb.valtype() - basic diagnostic print function for evaluating value and type of target
 func valtype(label: String, targ: Variant) -> void:
 	print(label, ": ", targ, " [", typeof(targ), "]")
