7d1b98853a7faabfacd69ad3cd55e713b0bde4e5
mura glim <muraglim@gmail.com>
2026-06-01 14:39:43 -0500

    chore: cleanup nav_dest_Store stale reference
    boot_scene, template_scene, roll_scene deprecated keys and references removed.

--- stores/nav_dest_Store.gd
+++ stores/nav_dest_Store.gd
@@ -3,9 +3,6 @@ extends Store
 
 func _ready() -> void:
 	data = {
-		"boot_scene": "uid://b27eqwa55glmf",
-		"template_scene": "uid://sew0lmp877f3",
-		"roll_scene": "uid://c431n7ciwstmp",
 		"nav_check_channel": "uid://cpansnhgsaagn",
 		"nav_check_target_channel": "uid://yc3038a0dolg",
 		"nav_check_daemon": "uid://v6kjkq4xp532",
