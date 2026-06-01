0c90978a7338ce16ca91c95e3177a05372820a00
mura glim <muraglim@gmail.com>
2026-06-01 12:31:57 -0500

    fix: nav_Store references
    fix for nav_check_channel and nav_check_target_channel

--- stores/nav_store.gd
+++ stores/nav_store.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 		"boot_scene": "uid://b27eqwa55glmf",
 		"template_scene": "uid://sew0lmp877f3",
 		"roll_scene": "uid://c431n7ciwstmp",
-		"nav_checker": "uid://cpansnhgsaagn",
-		"nav_check_channel_target": "uid://yc3038a0dolg",
+		"nav_check_channel": "uid://cpansnhgsaagn",
+		"nav_check_target_channel": "uid://yc3038a0dolg",
 		"nav_check_daemon": "uid://v6kjkq4xp532",
 	}
