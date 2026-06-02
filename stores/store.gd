9ccb89a8a4155a3ffe453f29c5ec3979c1cda7e0
mura glim <muraglim@gmail.com>
2026-06-02 18:53:46 -0500

    style: Store
    establish logging convention mirroring language syntax
    clean up overly verbose and redundant logging

--- stores/store.gd
+++ stores/store.gd
@@ -2,60 +2,50 @@ class_name Store
 extends Node
 var data = {}
 
-@export var verbose := true
-
-func _log(msg: String) -> void:
-	if verbose:
-		print("[Store] ", msg)
-
-func set_value(key: Variant, value: Variant) -> void:
-	# MIN: data[key] = value
-	_log("set_value(key=%s, value=%s)" % [str(key), str(value)])
-	var previous = data.get(key, null)
-	data[key] = value
-	_log("  -> previous: %s" % str(previous))
+@export var verbose := false
 
 func get_value(key: Variant, default_value = null) -> Variant:
-	# MIN: return data.get(key, default_value)
-	_log("get_value(key=%s, default=%s)" % [str(key), str(default_value)])
 	if data.has(key):
 		var value = data[key]
-		_log("  -> found: %s" % str(value))
+		_log("get_value(key: %s) -> found: %s" % [key, value])
 		return value
-	_log("  -> not found, returning default: %s" % str(default_value))
+	_log("get_value(key: %s) -> not found, returning default: %s" % [key, default_value])
 	return default_value
 
-func append_value(key: Variant, value: Variant) -> void:
-	# MIN: 
-	#   if not data.has(key): data[key] = []
-	#   data[key].append(value)
-	_log("append_value(key=%s, value=%s)" % [str(key), str(value)])
-	if not data.has(key):
-		data[key] = []
-		_log("  -> created new array")
-	elif not data[key] is Array:
-		_log("  -> ERROR: key exists but is not Array (type: %s)" % typeof_string(data[key]))
-		push_error("append_value on non-array key: %s" % key)
-		return
-	data[key].append(value)
-	_log("  -> array now has %d items: %s" % [data[key].size(), str(data[key])])
+func set_value(key: Variant, value: Variant) -> void:
+	var previous = data.get(key, null)
+	data[key] = value
+	_log("set_value(key: %s, value: %s) -> previous: %s" % [key, value, previous])
 
 func clear_value(key: Variant) -> void:
-	# MIN: data.erase(key)
-	_log("clear_value(key=%s)" % str(key))
 	if data.has(key):
 		var old = data[key]
 		data.erase(key)
-		_log("  -> erased value: %s" % str(old))
+		_log("clear_value(key: %s) -> erased value: %s" % [key, old])
 	else:
-		_log("  -> key not found, nothing to clear")
+		_log("clear_value(key: %s) -> key not found" % key)
+
+func append_value(key: Variant, value: Variant) -> void:
+	if not data.has(key):
+		data[key] = []
+	elif not data[key] is Array:
+		push_error("append_value(key: %s): key exists but is type %s" % [key, typeof_string(data[key])])
+		return
+	data[key].append(value)
+	_log("append_value(key: %s) -> Array now has %d items: %s" % [key, data[key].size(), data[key]])
 
 func has_key(key: Variant) -> bool:
-	# MIN: return data.has(key)
-	_log("has_key(key=%s)" % str(key))
 	var exists = data.has(key)
-	_log("  -> returns %s" % str(exists))
+	_log("has_key(key: %s) -> returns %s" % [key, exists])
 	return exists
+
+func get_keys() -> Array:
+	_log("get_keys() -> has %d keys" % data.size())
+	return data.keys()
+
+func _log(msg: String) -> void:
+	if verbose:
+		print("[%s] " % name, msg)
 
 func typeof_string(value: Variant) -> String:
 	return type_string(typeof(value))
