b78a355a84855cbc82b7c7a7612384acf86ca519
mura glim <muraglim@gmail.com>
2026-06-01 19:31:37 -0500

    feat: add verification for daemon_init() in _nav_check_Store
    add as part of routing logic for nav_check_channel - fires verification on daemon_init()

--- daemons/nav_check_daemon.gd
+++ daemons/nav_check_daemon.gd
@@ -2,4 +2,5 @@ extends Daemon
 
 func daemon_init() -> void:
 	await get_tree().process_frame
-	Nav.evict_back_channel(self, get_nav("nav_check_channel_target"))
+	Keeper.get_store("_nav_check_store").nav_check_stamp("verify_daemon_init")
+	Nav.evict_back_channel(self, get_nav("nav_check_target_channel"))
