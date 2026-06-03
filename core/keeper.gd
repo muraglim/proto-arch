08b68e48b4aac5bb630fc20ee10d41a124601ac4
mura glim <muraglim@gmail.com>
2026-06-02 19:12:04 -0500

    style: Keeper
    conform to logging convention mirroring language syntax
    fix default_value parameter passthrough to Store

--- core/keeper.gd
+++ core/keeper.gd
@@ -13,52 +13,52 @@ func _ready() -> void:
 	for child in get_children():
 		stores[child.name.to_lower()] = child
 
-func get_value(store_node: String, key: String) -> Variant:
+func get_value(store_node: String, key: String, default_value = null) -> Variant:
 	if not stores.has(store_node):
-		push_error("[Keeper] get_value(): unknown store - " + store_node)
+		push_error("[Keeper] get_value(store: %s, key: %s, default_value: %s): unknown store" % [store_node, key, default_value])
 		return null
-	return stores[store_node].get_value(key)
+	return stores[store_node].get_value(key, default_value)
 
 func set_value(store_node: String, key: String, value: Variant) -> void:
 	if not stores.has(store_node):
-		push_error("[Keeper] set_value(): unknown store - " + store_node)
+		push_error("[Keeper] set_value(store: %s, key: %s, value: %s): unknown store" % [store_node, key, value])
 		return
 	stores[store_node].set_value(key, value)
 
 func clear_value(store_node: String, key: String) -> void:
 	if not stores.has(store_node):
-		push_error("[Keeper] clear_value(): unknown store - " + store_node)
+		push_error("[Keeper] clear_value(store: %s, key: %s): unknown store" % [store_node, key])
 		return
 	stores[store_node].clear_value(key)
 
 func append_value(store_node: String, key: String, value: Variant) -> void: 
 	if not stores.has(store_node):
-		push_error("[Keeper] append_value(): unknown store - " + store_node)
+		push_error("[Keeper] append_value(store: %s, key: %s, value: %s): unknown store" % [store_node, key, value])
 		return
 	stores[store_node].append_value(key, value)
 
 func has_key(store_node: String, key: String) -> bool:
 	if not stores.has(store_node):
-		push_error("[Keeper] has_key(): unknown store - " + store_node)
+		push_error("[Keeper] has_key(store: %s, key: %s): unknown store" % [store_node, key])
 		return false
 	return stores[store_node].has_key(key)
 
 func get_keys(store_node: String) -> Array:
 	if not stores.has(store_node):
-		push_error("[Keeper] get_keys(): unknown store - " + store_node)
+		push_error("[Keeper] get_keys(store: %s): unknown store" % store_node)
 		return []
 	return stores[store_node].get_keys()
 
 func get_store(store_node: String) -> Node:
 	if not stores.has(store_node):
-		push_error("[Keeper] get_store(): unknown store - " + store_node)
+		push_error("[Keeper] get_store(store: %s): unknown store" % store_node)
 		return null
 	return stores[store_node]
 
 func call_store(store_node: String, method: String, args: Array = []) -> Variant:
 	#TODO edge case utility, no current use case
 	if not stores.has(store_node):
-		push_error("[Keeper] call_store(): unknown store - " + store_node)
+		push_error("[Keeper] call_store(store: %s, method: %s, args: %s): unknown store" % [store_node, method, args])
 		return null
 	return stores[store_node].callv(method, args)
 
