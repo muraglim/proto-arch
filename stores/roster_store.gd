9b0b0fb8f074be85d5aed094175a88b7814f711d
mura glim <muraglim@gmail.com>
2026-06-03 14:19:30 -0500

    fix: roster_store id type -> String

--- stores/roster_store.gd
+++ stores/roster_store.gd
@@ -1,6 +1,6 @@
 extends Store
 
-func add_to_roster(named: String, id: int, gender: bool, origin: String, zodiac: float) -> void:
+func add_to_roster(named: String, id: String, gender: bool, origin: String, zodiac: float) -> void:
 	if has_key(id):
 		push_error("Roster_Store: id already exists - %s" % str(id))
 		return
