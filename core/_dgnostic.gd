c7d7912cd28e18bf1564bf03da593fe040d71bed
mura glim <muraglim@gmail.com>
2026-06-11 02:01:53 -0500

    chore: remove _dGnostic

--- core/_dgnostic.gd
+++
@@ -1,8 +0,0 @@
-## _dGnostic
-## Stub autoload reserved for diagnostic and state-tracking infrastructure.
-##
-## Intended for a later stage of development when gameplay complexity
-## warrants more systematic inspection than _pComb print calls provide.
-## Add to this when the pain point arrives, not before.
-
-extends Node
