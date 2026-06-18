78fd26b6d9d7e9c0dbe5ce4d5e3391bd6fcc08a6
mura glim <muraglim@gmail.com>
2026-06-17 21:24:18 -0500

    feat: add Geist lifecycle method stubs, _log() method, verbose export var

--- core/geist.gd
+++ core/geist.gd
@@ -1,2 +1,14 @@
 class_name Geist
 extends Node
+
+@export var verbose := false
+
+func geist_init() -> void:
+	pass
+
+func geist_shutdown() -> void:
+	pass
+
+func _log(msg: String) -> void:
+	if verbose:
+		print("[%s] " % name, msg)
