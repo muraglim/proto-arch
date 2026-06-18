69465ff34666cac99fce60ce99cb053a37113c9c
mura glim <muraglim@gmail.com>
2026-06-17 22:16:06 -0500

    feat: add ConsoleChannel
    generic RichTextLabel/LineEdit display surface, replaces per-context channel scripts

---
+++ channels/console_channel.gd
@@ -0,0 +1,12 @@
+class_name ConsoleChannel
+extends Control
+
+@onready var output: RichTextLabel = $MarginContainer/VBoxContainer/Output
+@onready var input: LineEdit = $MarginContainer/VBoxContainer/Input
+
+func channel_show() -> void:
+	set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
+	input.grab_focus()
+
+func set_input_max_length(value: int) -> void:
+	input.max_length = value
