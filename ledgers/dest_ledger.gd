2b4c65d4f79def5748c333b7855d19137fef303b
mura glim <muraglim@gmail.com>
2026-06-20 04:52:14 -0500

    refactor: _nav_dest_Ledger -> dest_ledger, nested -> flat dict
    renamed due to lost association with now deprecated Nav autoload.
    replaced nested dict structure with flat dict, there is no longer a type field which is what necessitated nesting.

---
+++ ledgers/dest_ledger.gd
@@ -0,0 +1,11 @@
+class_name DestLedger
+extends Ledger
+
+func _ready() -> void:
+	data = {
+		"project_start_lens": "uid://bqje1v5xanflf",
+		"profile_lens": "uid://cqcgxkixst232",
+		"profile_daemon": "uid://balx8a65n0oca",
+		"console_medium": "uid://n1giitvbri08",
+		"console_channel": "uid://bxh8a4u7p5usq",
+}
